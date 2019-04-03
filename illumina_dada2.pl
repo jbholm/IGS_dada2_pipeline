a590207973395c5c75641a07b055d8b409cccfdc
Jon Lim <jlim7@comcast.net>
2019-04-03 11:15:22 -0400

    Run the whole pipeline if no -dbg options given (v1.0.4-multiple_dbg+3)
    Also, print DADA2 R CMD BATCH command to STDOUT (v1.0.4-multiple_dbg+5)

--- illumina_dada2.pl
+++ illumina_dada2.pl
@@ -337,13 +337,13 @@ print $logFH "$time\n";
 
 if(@dbg) {
     print "DBG FLAGS: ";
-    print $logFH, "DBG FLAGS: ";
+    print $logFH "DBG FLAGS: ";
     for (@dbg) {
         print "$_ ";
-        print $logFH, "$_ ";
+        print $logFH "$_ ";
     }
     print "\n";
-    print $logFH, "\n";
+    print $logFH "\n";
 }
 
 if ( ( !@dbg ) || grep( /^qiime_and_validation$/, @dbg ) ) {
@@ -392,7 +392,7 @@ if ( ( !@dbg ) || grep( /^qiime_and_validation$/, @dbg ) ) {
     } else {
         die "validate_mapping_file.py did not produce an error log";
     }
-    if ( !grep( /^extract_barcodes$/, @dbg ) ) {
+    if ( @dbg && !grep( /^extract_barcodes$/, @dbg ) ) {
         die 
 "Finished printing QIIME configuration and validating mapping file. Terminated "
 . "because -dbg extract_barcodes was not specified.";
@@ -643,7 +643,7 @@ if ( ( !@dbg ) || grep( /^extract_barcodes$/, @dbg ) ) {
           if !$dryRun;
     }
 
-    if ( !grep( /^demultiplex$/, @dbg ) ) {
+    if ( @dbg && !grep( /^demultiplex$/, @dbg ) ) {
         die 
         "Finished extracting barcodes and demultiplexing libraries. Terminated "
 . "because -dbg demultiplex was not specified.";
@@ -925,7 +925,7 @@ if ( !@dbg || grep( /^demultiplex$/, @dbg ) ) {
         print $logFH "$newSamNo sample-specific files present as expected.\n";
     }
 
-    if ( !grep( /^tagclean$/, @dbg ) ) {
+    if ( @dbg && !grep( /^tagclean$/, @dbg ) ) {
         die
 "Finished extracting barcodes and demultiplexing libraries. Terminated "
 . "because -dbg tagclean was not specified.";
@@ -1185,7 +1185,7 @@ if ( !@dbg || grep( /^tagclean$/, @dbg ) ) {
           . "as expected. Beginning DADA2.\n";
     }
 
-    if ( !grep( /^dada2$/, @dbg ) ) {
+    if ( @dbg && !grep( /^dada2$/, @dbg ) ) {
         die
 "Finished extracting barcodes and demultiplexing libraries. Terminated "
 . "because -dbg dada2 was not specified.";
@@ -1494,6 +1494,7 @@ while (<$logFH>) {
           if !$dryRun;
     }
 }
+print $logFH "\n";
 close $logFH;
 
 ## moving final files to directory created within /local/projects/16S_DATA/projects/
@@ -1601,6 +1602,7 @@ sub run_R_script {
     close OUT;
 
     my $cmd = "$R CMD BATCH $outFile";
+    print "$cmd";
     system($cmd) == 0 or die "system($cmd) failed:$?\n";
 
     my $outR = $outFile . "out";
