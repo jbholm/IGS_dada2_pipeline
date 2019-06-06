9acaa54c07aa60dd62afd7c775c8642c2ca1f1c0
Jon Lim <jlim7@comcast.net>
2019-06-06 16:21:41 -0400

    Bugfix: Allow custom qsub args to override the defaults (v2.1.4-fix_qsub_arg_overriding+1)
    Accomplished by placing custom qsub args after the defaults, not
    before them.

--- part1.sh
+++ part1.sh
@@ -327,7 +327,7 @@ OPTSARR=("$DADA2" "$DBG" "$VERBOSE" "$DRY_RUN" "$ONESTEP" "$PARAMS")
 OPTS="${OPTSARR[*]}"
 OPTS="$( echo "$OPTS" | awk '{$1=$1;print}' )"
 
-ARGS=("$QSUB_ARGS" "-cwd" "-b y" "-l mem_free=200M" "-P" "$QP" "-q threaded.q" "-pe thread 4" "-V" "-o ${SD}/qsub_stdout_logs/illumina_dada2.pl.stdout" "-e ${SD}/qsub_error_logs/illumina_dada2.pl.stderr" "${MY_DIR}/illumina_dada2.pl" "$INPUT" "-wd" "$SD" "-v" "$VAR" "-m" "$MAP" "$OPTS")
+ARGS=("-cwd" "-b y" "-l mem_free=200M" "-P" "$QP" "-q threaded.q" "-pe thread 4" "-V" "-o ${SD}/qsub_stdout_logs/illumina_dada2.pl.stdout" "-e ${SD}/qsub_error_logs/illumina_dada2.pl.stderr" "$QSUB_ARGS" "${MY_DIR}/illumina_dada2.pl" "$INPUT" "-wd" "$SD" "-v" "$VAR" "-m" "$MAP" "$OPTS")
 CMD=()
 for ARG in "${ARGS[@]}"; do
     if [[ -n "$ARG" ]]; then
