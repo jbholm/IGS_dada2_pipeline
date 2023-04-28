88edc65cae2eeaa2fd258680e2319691176a7668
Jon Lim <jlim7@comcast.net>
2023-04-28 11:20:11 -0400

    Use project manifest to rename samples from runs (v5.3.3)
    ./scripts/make_project_sample_map.py
      Any samples in wells E12-H12 of any plate are treated as MSL controls.
      If provided, use the manifest to rename samples in the gDNA plates. Exclude
        the control wells from renaming. Quit if any samples in the manifest are
        absent from the gDNA plate maps.
      Report the number of unfilled wells in the gDNA plate maps, and the final
        numer of client samples and MSL controls.
    
    Squashed commit of the following:
    
    commit b9c5f6fb044f053b75fb38fd8c234d9bf6529767
    commit 3cb1c5ff546988f060300858a7c3017e7aebb290

--- scripts/make_project_sample_map.py
+++ scripts/make_project_sample_map.py
@@ -8,28 +8,69 @@ from pathlib import Path
 CONFIG = {
 	"RUN_PLATE_FORMATTERS": {
 		"IDT for Illumina": lambda row, run: ".".join([run, "IDT" + row["primer plate name"].split("XTR_Plate ")[1]]),
-		"Nextera XT": lambda row, run: ".".join([run, "UDI" + row["XT barcode plate"].split("set ")[1]])
+		"Nextera XT": lambda row, run: ".".join([run, row["XT barcode plate"].replace("set ", "UDI")])
 	},
-	"RUN_FORMATTER": lambda x: re.sub(pattern="[^.a-zA-Z0-9]", repl=".", string=x)
+	"RUN_FORMATTER": lambda x: re.sub(pattern="[^.a-zA-Z0-9]", repl=".", string=x),
+	"CTRL_WELLS": ["E12", "F12", "G12", "H12"]
 }
 
 def main(args):
-	gdna_to_barcode_plate = get_pooling(args.pooling)
-
 	# main columns: ["gDNA plate ID", "WELL", "SAMPLE ID"]
 	sample_to_gdna = get_gdna(args.gdna)
-	sample_to_gdna = sample_to_gdna.loc[-pd.isna(sample_to_gdna["SAMPLE ID"]), ]
-
-	project_map = gdna_to_barcode_plate.merge(sample_to_gdna, how="inner")
-	join_fun = lambda row: ".".join([row["RUN.PLATE"], row["WELL"]])
-	project_map["RUN.PLATEPOSITION"] = project_map.apply(join_fun, axis=1)
-	project_map = project_map.rename(columns={"SAMPLE ID": "sampleID"})
-	project_map = project_map[['RUN.PLATEPOSITION', "sampleID"]]
-
+	blanks = pd.isna(sample_to_gdna["SAMPLE ID"])
+	print("Removing " + str(blanks.value_counts()[True]) + " wells with no sample. If this is too many, check that the 'SAMPLE ID' column is filled.")
+	sample_to_gdna = sample_to_gdna.loc[~blanks, ]
+	
+	#print(sample_to_gdna)
 	if(args.manifest):
-		project_map = project_map.merge(get_manifest(args.manifest))
+		sample_to_gdna = apply_manifest(sample_to_gdna, args.manifest)
 
+	gdna_to_barcode_plate = get_pooling(args.pooling)
+	project_map = gdna_to_barcode_plate.merge(sample_to_gdna, how="inner")
+	if sample_to_gdna.shape[0] > project_map.shape[0]:
+		quit("Lost rows when joining gDNA maps to pooling detail. gDNA plate IDs did not match.")
+
+	project_map.loc[:, "RUN.PLATEPOSITION"] = project_map.apply(
+		lambda row: ".".join([row["RUN.PLATE"], row["WELL"]]), 
+		axis=1
+		)
+	project_map = project_map.rename(columns={"SAMPLE ID": "sampleID"})
+	project_map["sampleID"] = project_map["sampleID"].str.replace("\\s", "_", regex=True)
+
+	print("Final counts:")
+	counts = (project_map["WELL"]
+       	.isin(CONFIG["CTRL_WELLS"])
+		.value_counts())
+	counts.index = counts.index.map({False: "Client samples", True: "MSL Controls"})
+	for index in counts.index:
+		print(str(index) + "\t" + str(counts.loc[index]))
+
+	project_map = project_map[['RUN.PLATEPOSITION', "sampleID"]]
+	
 	project_map.to_csv(args.output, sep="\t", index=False)
+
+
+
+def apply_manifest(sample_to_gdna, manifest_file):
+	sample_to_gdna = sample_to_gdna.copy()
+	manifest = get_manifest(manifest_file)["sample_name"]
+
+	# detach controls temporarily
+	ctrls_bool = sample_to_gdna["WELL"].isin(CONFIG["CTRL_WELLS"])
+	ctrls = sample_to_gdna.loc[ctrls_bool, :]
+	sample_to_gdna = sample_to_gdna.loc[~ctrls_bool, :]
+
+	not_in_gDNA = ~manifest.index.isin(sample_to_gdna["SAMPLE ID"].values)
+	sample_to_gdna["SAMPLE ID"] = sample_to_gdna.loc[:, "SAMPLE ID"].map(manifest)
+	if(any(not_in_gDNA)):
+		print("Manifest samples missing from gDNA maps:")
+		print(manifest.loc[list(not_in_gDNA)])
+		quit()
+
+	sample_to_gdna = pd.concat([sample_to_gdna, ctrls]) # add back the controls
+	return sample_to_gdna
+
+
 
 def get_pooling(indir):
 	# load config from json??
@@ -43,21 +84,18 @@ def get_pooling(indir):
 		# "124 Ill_2step_XTR_16S PCR MSL_NS_124 Pooling detail 1-11-2023.xlsx"
 		# "/local/projects-t3/MSL/pipelines/test/pooling_detail/118 Ill_2step XT_16S PCR MSL_MS_118 Pooling detail 10-20-2022.xlsx"
 		pooling_df = pd.read_excel(pooling_file, skiprows=1, header=None, dtype=str)
+		# could also get run name from the "Illumina 16S PCR pool ID" column (column 11)
 		run = pooling_df.iloc[0, 4]
 		run = CONFIG["RUN_FORMATTER"](run)
 
 		# remove unneeded rows and one empty column
 		pooling_df = pooling_df.iloc[3:, 1:]
-
+		# get header
 		pooling_df.columns = pooling_df.iloc[0, :]
 		pooling_df = pooling_df.iloc[1:, :]
 		
-		# remove odd rows that occur because of merged rows
-		# any way to do this with just [] not iloc?
-		pooling_df = pooling_df.iloc[np.arange(int(pooling_df.shape[0] / 2)) * 2, :]
-
-		# remove rows we aren't interested in
-		pooling_df = pooling_df.loc[-pd.isna(pooling_df["gDNA plate ID"]), :]
+		pooling_df = (pooling_df.iloc[np.arange(int(pooling_df.shape[0] / 2)) * 2, :] # remove odd rows
+			.loc[-pd.isna(pooling_df["gDNA plate ID"]), :]) # remove rows with empty field
 
 		# get col immediately left of this column
 		maybe_idt_plates = pooling_df.iloc[:, pooling_df.columns.get_loc("Sample Description") - 1]
@@ -68,11 +106,19 @@ def get_pooling(indir):
 			pooling_df["RUN.PLATE"] = pooling_df.apply(CONFIG["RUN_PLATE_FORMATTERS"]["IDT for Illumina"], axis=1, run=run) 
 			pooling_df = pooling_df[["RUN.PLATE", "gDNA plate ID"]]
 			
-		elif pooling_df.columns[1] == "XT barcode plate" :
-			if all([re.search("^set [A-D]$", plate) for plate in pooling_df["XT barcode plate"]]):
-				# process as XT UDI plates
+		elif (pooling_df.columns[1] == "XT barcode plate" 
+			and all([re.search("^set [A-D]$", plate) for plate in pooling_df["XT barcode plate"]])):
+				# process as Nextera XT UDI plates
 				pooling_df["RUN.PLATE"] = pooling_df.apply(CONFIG["RUN_PLATE_FORMATTERS"]["Nextera XT"], axis=1, run=run) 
 				pooling_df = pooling_df[["RUN.PLATE", "gDNA plate ID"]]
+		else:
+			msg = "Unable to detect UDI plate configuration from pooling: " + pooling_file
+			msg = msg + """
+				Acceptable configurations:
+					IDT for Illumina: The column left of "Sample Description" has values that match the pattern "^XTR_Plate.*"
+					Nextera XT: The third column is called "XT barcode plate" and has values that match the pattern "^set [A-D]$"
+			"""
+			quit(msg)
 
 		pooling_dfs.append(pooling_df)
 
@@ -82,21 +128,20 @@ def get_gdna(indir):
 	gdna_dfs = []
 
 	for gdna_file in indir.iterdir():
-		# get gDNA plate IDs, IDT plate numbers, and run name
-		
-		# "124 Ill_2step_XTR_16S PCR MSL_NS_124 Pooling detail 1-11-2023.xlsx"
-		# "/local/projects-t3/MSL/pipelines/test/pooling_detail/118 Ill_2step XT_16S PCR MSL_MS_118 Pooling detail 10-20-2022.xlsx"
 		gdna_df = pd.read_excel(gdna_file, header=0)
 		gdna_plate = str(gdna_file.name)
 		gdna_plate_parts = gdna_plate.split("_")
 		gdna_plate = gdna_plate_parts[len(gdna_plate_parts)-1].split(".")[0]
 		gdna_df["gDNA plate ID"] = gdna_plate
 		gdna_dfs.append(gdna_df)
-	
-	return pd.concat(gdna_dfs)
+	gdna_df = pd.concat(gdna_dfs)
+
+	return gdna_df
 
 def get_manifest(filepath):
-	return pd.DataFrame()
+	manifest_df = pd.read_excel(filepath, skiprows=14, header=0, index_col=1)
+
+	return manifest_df
 
 if __name__=="__main__":
 	ap = argparse.ArgumentParser()
@@ -104,6 +149,7 @@ if __name__=="__main__":
 	ap.add_argument("--pooling", "-p", type=Path, required=False, default=Path("./pooling"))
 	ap.add_argument("--gdna", "-g", type=Path, required=False, default=Path("./gDNA_maps"))
 	ap.add_argument("--manifest", "-m", type=Path, required=False)
+	# parser.add_argument('--strict', action=argparse.BooleanOptionalAction)
 
 	ap.add_argument("--output", "-o", type=Path, required=False, default=Path("./project_map.txt"))
 
